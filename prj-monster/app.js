10b595baacc4fa28c43cfdfeda7e701fe0140f78
Alex Mwangi <alex@stl-horizon.com>
2022-06-02 13:18:23 +0300

    Add healPlayer() that gets a random value and add to player's health- Should not exceed 100 ; Monster will still attack

--- prj-monster/app.js
+++ prj-monster/app.js
@@ -50,6 +50,19 @@ const app = Vue.createApp({
             const attackValue = getRandomValue(10,25)
             this.monsterHealth  -= attackValue
             this.attackPlayer()
+        } ,
+
+        healPlayer(){
+            this.currentRound++
+            const healValue = getRandomValue(8,20)
+            // We should not heal ourelves more than a 100
+            if (this.playerHealth + healValue > 100 ){
+                this.playerHealth = 100
+            }else{
+
+                this.playerHealth += healValue
+            }
+            this.attackPlayer()
         }
 
     },
