7eb0bba036bb342ffd2733b86af4e02815bc0db4
Alex Mwangi <alex@stl-horizon.com>
2022-06-02 13:02:40 +0300

    Added currentRounds data prop for special attack; Added mayUseSpecialAttack() as computed property to track currentRounds value that should not exceed 3

--- prj-monster/app.js
+++ prj-monster/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
         return {
             playerHealth : 100,
             monsterHealth : 100,
+            currentRound : 0,
         }
     } ,
 
@@ -18,16 +19,21 @@ const app = Vue.createApp({
         },
         playerBarStyles(){
             return { width: this.playerHealth + '%' }
-        }
+        },
+
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !== 0
+        },
+
     },
 
     methods : {
 
         attackMonster(){
-
+            this.currentRound++
             const attackValue = getRandomValue(5 , 12)
-            this.monsterHealth = this.monsterHealth - attackValue
-            //shorthand: this.monsterHealth -= attackValue
+            this.monsterHealth -= attackValue
+            //longhand: this.monsterHealth = this.monsterHealth - attackValue
             // Monster to attack Player
             this.attackPlayer()
         },
@@ -35,8 +41,16 @@ const app = Vue.createApp({
         attackPlayer() {
 
             const attackValue = getRandomValue(8, 15)
-            this.playerHealth = this.playerHealth - attackValue
+            this.playerHealth -= attackValue
         },
+
+        // We can only do it with 3 rounds
+        specialAttackMonster(){
+            this.currentRound++
+            const attackValue = getRandomValue(10,25)
+            this.monsterHealth  -= attackValue
+            this.attackPlayer()
+        }
 
     },
 
